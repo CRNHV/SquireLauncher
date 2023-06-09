8dae8a803dd09810de778ea8876a9bbd10172ee2
CRNHV <noreply@example.com>
2023-06-09 23:54:33 +0200

    Add new entity for holding settings

---
+++ SquireLauncher.Gui/Data/Entities/Settings.cs
@@ -0,0 +1,10 @@
+﻿namespace SquireLauncher.Gui.Data.Entities;
+
+public class Settings
+{
+    public int Id { get; set; }
+    public string? JavaExePath { get; set; }
+    public string? SquireJarPath { get; set; }
+    public string? SquirePath { get; set; }
+    public string? Branch { get; set; }
+}
