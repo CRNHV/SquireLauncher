fe8d73ae35468207d49776dac7a09ef07045465f
CRNHV <noreply@example.com>
2023-06-09 23:55:14 +0200

    Add new implementation for launcher settings

---
+++ SquireLauncher.Gui/Launcher/ClientLauncher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquireLauncher.Gui.Launcher;
+
+internal class ClientLauncher : IClientLauncher
+{
+    public ClientLauncher()
+    {
+
+    }
+
+    public async Task LaunchClientAsync(LaunchSettings launchSettings)
+    {
+        Process process = new Process();
+        process.StartInfo.FileName = launchSettings.JavaExeLocation;
+        process.StartInfo.Arguments = launchSettings.JavaArguments + " " + launchSettings.SquireJarLocation + " " + launchSettings.SquireArguments;
+        process.StartInfo.UseShellExecute = true;
+        process.Start();
+    }
+}
---
+++ SquireLauncher.Gui/Launcher/LaunchSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace SquireLauncher.Gui.Launcher;
+
+internal class LaunchSettings
+{
+    public string JavaExeLocation { get; set; }
+    public string JavaArguments { get; set; } = "-XX:+DisableAttachMechanism -Xmx4G -Xss2m -XX:CompileThreshold=1500 -jar -Drunelite.launcher.nojvm=true";
+    public string SquireJarLocation { get; set; }
+    public string SquireArguments { get; set; }
+    public string Branch { get; internal set; }
+}
